e4bd5e7efa034826f5ebaf222f4dc955027aab5f
Stanisław Tabisz <92331225+tabiszs@users.noreply.github.com>
2022-04-23 12:56:42 +0200

    Update morse.mk

--- buildroot/package/morse/morse.mk
+++ buildroot/package/morse/morse.mk
@@ -4,10 +4,17 @@
 #
 ################################################################################
 
-MORSE_VERSION = 1.0
-MORSE_SITE = $(TOPDIR)/../board/package/morse
+MORSE_VERSION = 2.0
+MORSE_SITE = $(TOPDIR)/../board/pw/lab4/morse
 MORSE_SITE_METHOD = local
 MORSE_DEPENDENCIES = libgpiod
+
+define MORSE_BUILD_CMDS
+   $(MAKE) $(TARGET_CONFIGURE_OPTS) morse -C $(@D)
+endef
+define MORSE_INSTALL_TARGET_CMDS 
+   $(INSTALL) -D -m 0755 $(@D)/morse $(TARGET_DIR)/usr/bin 
+endef
 MORSE_LICENSE = Proprietary
 
-$(eval $(cmake-package))
+$(eval $(generic-package))
